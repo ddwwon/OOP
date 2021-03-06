410849174c128517c185daddceb79914cb815eed
dowon <70639119+ddwwon@users.noreply.github.com>
2021-03-06 21:12:26 +0900

    1주차
    더 알고싶으면 study repository에서 찾아보기

---
+++ ABC/ABC/ABC.h
@@ -0,0 +1,38 @@
+﻿
+// ABC.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CABCApp:
+// 이 클래스의 구현에 대해서는 ABC.cpp을(를) 참조하세요.
+//
+
+
+class CABCApp : public CWinApp
+{
+public:
+	CABCApp();
+
+// 재정의입니다.(오버라이딩): 원래 CWinApp에 있던것을 상속받아서 재선언함(InitInstance)
+public:
+	
+	// InitInstance: 현재 프로그램이 만들어질때 자동적으로 실행되도록
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+// theApp: MFC에서 가장 중요한것, 전역변수 하지만 클래스야!! 
+// ABC.cpp에 저장되어 있고 이파일에서는 참조한 것
+// CABCApp라고 실체를 하나 만드는데 이름을 theApp로 한 것 (야간 구조체 너낌)
+extern CABCApp theApp;
---
+++ ABC/ABC/ABCDlg.h
@@ -0,0 +1,59 @@
+﻿
+// ABCDlg.h: 헤더 파일
+//
+
+#pragma once
+
+// class라고 쓴 것: 캡슐화(Encapsulation), 상속(Inheritance), 다형성(Polymorphism)
+// CABCDlg 대화 상자
+class CABCDlg : public CDialogEx
+{
+	// 생성입니다.
+public:
+
+	// 괄호가 있으면 맴버 함수, 없으면 맴버 변수
+	// CABCDlg: 데이터타입 ex) int i; 에서 int 같은 것
+	CABCDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	// 소멸자: 소멸자 이름은 클래스 이름과 같아야 하고 앞에 ~를 달아야 한다. 인수, 반환값 없음
+	 /*~CABCDlg()
+	{
+		 ...
+	}
+	 이렇게 사용*/
+
+	/*
+	public: 상속가능, 클래스 외부에서 접근 가능
+	private: 상속불가, 클래스 외부에서 접근 불가
+	protected: 상속가능, 클래스 외부에서 접근 불가
+	*/
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABC_DIALOG };
+#endif
+
+	protected:
+	// virtual: DoDataExchange 함수가 완전하게 만들어지지 않았다, 나중에 구현부에서 완벽하게 만들어야 한다, 가상함수 
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	// 맴버변수
+	HICON m_hIcon;
+
+	// 맴버 함수
+	// 생성된 메시지 맵 함수
+	// OnInitDialog: 우리가 알림창을 만들때 일일히 컨트롤안함. 대신 얘가 해줌
+	virtual BOOL OnInitDialog();
+	// OnSysCommand: 시스템메뉴를 누르면 자동적으로 실행되는 함수
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	// Onpaint: 화면의 크기가 변경되거나 화면이 겹쳐졌다가 다시 나타날때 재실행되는 함수
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+};
