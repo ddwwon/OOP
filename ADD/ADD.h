f4baf2e037f82b34a6004fe89c92977759ca4ee4
dowon <70639119+ddwwon@users.noreply.github.com>
2021-03-11 11:44:34 +0900

    2주차 - 1차시
    숫자 입력해서 더한 값 나오게

---
+++ ADD/ADD.h
@@ -0,0 +1,32 @@
+﻿
+// ADD.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CADDApp:
+// 이 클래스의 구현에 대해서는 ADD.cpp을(를) 참조하세요.
+//
+
+class CADDApp : public CWinApp
+{
+public:
+	CADDApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CADDApp theApp;
